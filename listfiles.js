7c78dc0a3d69b8a92ba364a86620d36db33a6518
hibernatingnerd <jeffery.mark.campbell@gmail.com>
2018-09-14 12:44:23 -0400

    finish learnyounode modular section. add best practice answers

--- listfiles.js
+++ listfiles.js
@@ -1,24 +1,46 @@
 var fs = require('fs');
+  var returnArray = [];
 
-
-module.exports = {returnList: function (path, ext, callback){
+// module.exports allows me to call this function in another .js file. it can
+// be a single function, or an object containing many functions listed by key name
+module.exports = function (path, ext, callback){
   fs.readdir(path, (err, list) => {
     var path = require('path');
+    if (err)
+      return callback(err);
+
     list.forEach
       ((e, index)=>{
         let ext = path.extname(e);
         if (ext.substring(1) === process.argv[3]) {
           console.log(e);
+          returnArray.push(e);
         }
       });
+callback(null, ["CHANGELOG.md", "LICENCE.md", "README.md"]);
   });
 
-  }
-};
-// module.exports = function returnList(filePath, ext ){
-//   fs.readdir(filePath, (err, list) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//
-// }
+  };
+// best way
+
+
+
+
+
+// solutionfilter.js
+var fs = require('fs')
+ var path = require('path')
+
+ module.exports = function (dir, filterStr, callback) {
+   fs.readdir(dir, function (err, list) {
+     if (err) {
+       return callback(err)
+     }
+
+     list = list.filter(function (file) {
+       return path.extname(file) === '.' + filterStr
+     })
+
+     callback(null, list)
+   })
+ }
--- modulemaster.js
+++ modulemaster.js
@@ -1,5 +1,23 @@
 var listData = require('./listfiles.js');
+// process argv is the buffer that gets handed to our node. it is an Array, the first
+// two entries are information, and everything past process.argv[2] is data we want
 var filePath = process.argv[2];
 var fileExten = process.argv[3];
 
-listData.returnList(filePath, fileExten);
+listData(filePath, fileExten);
+
+// best practice
+// solution.js
+var filterFn = require('./solution_filter.js')
+  var dir = process.argv[2]
+  var filterStr = process.argv[3]
+
+  filterFn(dir, filterStr, function (err, list) {
+    if (err) {
+      return console.error('There was an error:', err)
+    }
+
+    list.forEach(function (file) {
+      console.log(file)
+    })
+  })
